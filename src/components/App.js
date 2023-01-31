d8f246b1b2b466641c4546d8831a8f560963ec93
George Aldington <georgealdington@gmail.com>
2023-01-31 18:11:50 +0000

    cleans repo and adds logo and text input

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Search from "./Search";
 import "../styles/app.css";
 
 const App = () => {
   return (
     <div className="app">
-      <p>Hello World!</p>
+      <img className="nasa-logo"
+        src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
+        alt="red nasa logo"
+      />
+      <Search />
     </div>
   );
-}
+};
 
 export default App;
