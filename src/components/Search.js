98a31bf16390bba149540864c5f20959a592716b
George Aldington <georgealdington@gmail.com>
2023-02-02 22:09:26 +0000

    adds proptypes to Search and SearchResults components

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,28 +1,35 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 import getImages from "../requests/getImages";
 import "../styles/search.css";
 
-function Search( {setSearchResults }) {
+function Search({ setSearchResults }) {
   const [searchValue, setSearchValue] = useState("");
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(searchValue)
+    console.log(searchValue);
     setSearchResults(await getImages(searchValue));
   };
 
   return (
     <>
-    <form className="search-form" onSubmit={(event) => handleSubmit(event)}>
-      <input
-        onChange={(e) => setSearchValue(e.target.value)}
-        className="search-input"
-        type="text"
-        data-testid="search-input"
-      />
-      <button className="search-button" type="submit">?</button>
+      <form className="search-form" onSubmit={(event) => handleSubmit(event)}>
+        <input
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="search-input"
+          type="text"
+          data-testid="search-input"
+        />
+        <button className="search-button" type="submit">
+          ?
+        </button>
       </form>
     </>
   );
 }
+
+Search.propTypes = {
+  setSearchResults: PropTypes.func.isRequired,
+};
 
 export default Search;
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,14 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function SearchResults({ results }) {
   if (!results.length) {
     return <p>No results</p>;
   }
   return results.map((image) => {
-    return (
-    <img src={image}  alt="placeholder" />
-    )
+    return <img src={image} alt="placeholder" />;
   });
 }
 
+SearchResults.propTypes = {
+  results: PropTypes.array.isRequired,
+};
 export default SearchResults;
