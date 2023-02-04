403878cf305734caccd4770a0161c103e8f69f47
George Aldington <georgealdington@gmail.com>
2023-02-04 12:05:38 +0000

    styles search results

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,13 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "../styles/search-results.css";
 
 function SearchResults({ results }) {
   if (!results.length) {
-    return <p>Sorry, we couldn't find anything.</p>;
+    return <p className="no-images-message">Sorry, we couldn't find anything.</p>;
   }
-  return results.map((image) => {
-    return <img src={image} alt="placeholder" />;
-  });
+  return (
+    <div className="search-results">
+      {results.map((image, index) => {
+        return (
+          <img
+            className="search-results__image"
+            src={image}
+            alt="space_image"
+            key={index}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 SearchResults.propTypes = {
