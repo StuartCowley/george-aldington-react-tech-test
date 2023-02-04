b481ca84bc0b4763d301943ae5e9d630d8bfd2e9
George Aldington <georgealdington@gmail.com>
2023-02-04 11:44:33 +0000

    writes test for SearchResults

---
+++ src/__tests__/SearchResults.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import SearchResults from "../components/SearchResults";
+
+describe("SearchResults", () => {
+  let resultsMock = ["test1", "test2"];
+
+  it("renders results correctly", () => {
+    const { asFragment } = render(<SearchResults results={resultsMock} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders correctly if there are no results", () => {
+    resultsMock = [];
+    render(<SearchResults results={resultsMock} />);
+    const displayMessage = screen.getByText("Sorry, we couldn't find anything.");
+
+    expect(displayMessage).toBeInTheDocument();
+  });
+});
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 function SearchResults({ results }) {
   if (!results.length) {
-    return <p>No results</p>;
+    return <p>Sorry, we couldn't find anything.</p>;
   }
   return results.map((image) => {
     return <img src={image} alt="placeholder" />;
